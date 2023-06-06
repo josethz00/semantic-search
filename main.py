9595dfee74767b0f7bbd7fedf2801fee932801bd
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-06-06 18:03:24 -0300

    feat: add questions to a separate value

--- main.py
+++ main.py
@@ -24,15 +24,16 @@ if 'semantic-search' not in pinecone.list_indexes():
 index = pinecone.Index('semantic-search')
 
 quora_dataset = load_dataset('quora', split='train[240000:320000]')
-
-print(quora_dataset[:5])
+questions = [q['text'] for q in quora_dataset['questions']]
+questions = list(set(questions))  # remove duplicates
 
 batch_size = 32  # process everything in batches of 32
-for i in tqdm(range(0, len(quora_dataset['text']), batch_size)):
+for i in tqdm(range(0, len(questions), batch_size)):
     # set end position of batch
-    i_end = min(i+batch_size, len(quora_dataset['text']))
+    i_end = min(i+batch_size, len(quora_dataset['questions']))
     # get batch of lines and IDs
-    lines_batch = quora_dataset['text'][i: i+batch_size]
+    lines_batch = quora_dataset['questions'][i: i+batch_size][i]['text']
+    print(lines_batch)
     ids_batch = [str(n) for n in range(i, i_end)]
     # create embeddings
     res = openai.Embedding.create(input=lines_batch, engine=MODEL)
@@ -49,4 +50,4 @@ xq = openai.Embedding.create(input=query, engine=MODEL)['data'][0]['embedding']
 res = index.query([xq], top_k=5, include_metadata=True)
 
 for match in res['matches']:
-    print(f"{match['score']:.2f}: {match['metadata']['text']}")
+    print(f"{match['score']:.2f}: {match['metadata']['questions']}")
